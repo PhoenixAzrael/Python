aafc7c5cc0503d03d49b9d0547b37899b6ee42c0
PhoenixAzrael <christian.brueckner@live.com>
2017-09-11 17:39:39 +0200

    Create helloAPP.py
    First base from PyCharm

---
+++ helloAPP.py
@@ -0,0 +1,11 @@
+print('------------------------------------')
+print('                Hello APP')
+print('------------------------------------')
+print()
+
+# TODO: This need cleaned up.
+user_text = input('What is your name? ')
+# TODO: again clean up.
+greeting = 'Nice to meet you ' + user_text
+
+print(greeting)
